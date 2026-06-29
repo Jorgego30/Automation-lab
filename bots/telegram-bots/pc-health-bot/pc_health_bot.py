152f8c31e85fbc1b5b83acf921b1bba4712042bd
Jorgego30 <jorgegomgar5@gmail.com>
2026-06-29 14:00:01 +0200

    feat: start function creation

--- bots/telegram-bots/pc-health-bot/pc_health_bot.py
+++ bots/telegram-bots/pc-health-bot/pc_health_bot.py
@@ -11,8 +11,24 @@ load_dotenv()
 # Token id
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
-# Basic config for the loggin
+# Chat id
+ALLOWED_ID = int(os.getenv("TELEGRAM_CHAT_ID", "0"))
+
+# System logging configuration
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+
+# Start function creation
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Security filter
+    if not update.effective_chat or update.effective_chat.id != ALLOWED_ID:
+        logging.warning(f"Access denied.")
+        return
+
+    # 2. Responder al usuario autorizado
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="👋 Hello, Jorge! I'm your System Health bot. Use /status to check server status."
+    )
