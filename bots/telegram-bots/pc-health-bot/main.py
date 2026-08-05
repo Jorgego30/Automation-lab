b9be28496ca8a76ba74db22fac7a26e98576cd03
Jorgego30 <jorgegomgar5@gmail.com>
2026-08-05 11:13:13 +0200

    feat: add logs with logger to main.py

--- bots/telegram-bots/pc-health-bot/main.py
+++ bots/telegram-bots/pc-health-bot/main.py
@@ -1,14 +1,10 @@
 # Add principal libraries 
-import logging
 import handlers
+from logger import logger
 from config import TOKEN
 from telegram import Update
 from telegram.ext import ContextTypes 
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
-import socket
-
-# Disable IPv6 from sockets level of Python
-socket.has_ipv6 = False
 
 # Create button handler function to display all commands
 async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -16,30 +12,42 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     # Debug if not receive a query
     if not query:
+        logger.warning("CallbackQueryHandler received but query is None")
         return
 
     # Sends an alert to telegram about the reception of the click
     await query.answer()
-    
-    # Redirect each button to their function
-    if query.data == '/status':
-        await handlers.status(update, context)
-    elif query.data == '/uptime':
-        await handlers.uptime(update, context)
-    elif query.data == '/network':
-        await handlers.network(update, context)
-    elif query.data == '/top_processes':
-        await handlers.top_processes(update, context)
-    elif query.data == '/check:thresholds':
-        await handlers.check_thresholds(context)
+
+    user_id = update.effective_user.id if update.effective_user else "Unkown"
+    logger.info(f"Button press: '{query.data}' by user {user_id}")
+
+    try:
+        # Redirect each button to their function
+        if query.data == '/status':
+            await handlers.status(update, context)
+        elif query.data == '/uptime':
+            await handlers.uptime(update, context)
+        elif query.data == '/network':
+            await handlers.network(update, context)
+        elif query.data == '/top_processes':
+            await handlers.top_processes(update, context)
+        elif query.data == '/check:thresholds':
+            await handlers.check_thresholds(context)
+        else:
+            logger.warning(f"Callback data no recognized: {query.data}")
+    except Exception as e:
+            logger.error(f"Error touching button {query.data}: {e}", exc_info=True)
 
 if __name__ == '__main__':
     # Application creation with Bot Token
     application = ApplicationBuilder().token(TOKEN).build()
 
     if application.job_queue:
+        logger.info("Configurating JobQueue to check alerts")
         application.job_queue.run_repeating(handlers.check_thresholds, interval=10,first=3)
-
+    else:
+        logger.warning("JobQueue not available")
+        
     # Command creation (Handlers)
     application.add_handler(CommandHandler('start', handlers.start))
     application.add_handler(CallbackQueryHandler(button_handler))
@@ -49,5 +57,5 @@ if __name__ == '__main__':
     application.add_handler(CommandHandler('top_processes', handlers.top_processes))
 
     # Launch bot in polling mode
-    logging.info("Bot iniciado. Presiona Ctrl+C para detenerlo.")
+    logger.info("Bot running. Kill it with Ctrl+C")
     application.run_polling()
