ca16798a9b2398b62d1cc86acbc6c2780fc3f3bf
Jorgego30 <jorgegomgar5@gmail.com>
2026-08-26 09:37:36 +0200

    feat: add cash_boxes file to create later the funcion to see the cash boxes

---
+++ bots/telegram-bots/finance-bot/handlers/cash_boxes.py
@@ -0,0 +1,27 @@
+from config import ALLOWED_ID
+from logger import logger
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ContextTypes 
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
+
+async def cash_boxes(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id if update.effective_user else "Unkown"
+    # Security filter
+    if not update.effective_chat or update.effective_chat.id != ALLOWED_ID:
+        logger.warning(f"Denied access to /cash_boxes to the ID from user: {user_id}")
+        return
+
+    logger.info(f"Command /cash_boxes requested by user: {user_id}")
+
+    try:
+        # Send "typing" accion
+        await context.bot.send_chat_action(chat_id=update.effective_chat.id, action="typing")   
+
+        # Mostrar las huchas creadas con el dinero que hay en cada una, agregar boton de volver
+             
+        # Send the message
+        # await context.bot.send_message(text=report,chat_id=update.effective_chat.id, parse_mode="Markdown")
+        logger.info(f"Cash boxes sent correctly to {user_id}")
+
+    except Exception as e:
+        logger.error(f"Error obtaining or sending cash boxes: {e}")
