c04837d4e037bfa1ec96e09cdec20553681d2c46
Jorgego30 <jorgegomgar@gmail.com>
2026-09-20 09:50:07 +0200

    fix: fix import adding . before file names in from
    add .cash_boxes and .operations

--- bots/telegram-bots/finance-bot/handlers/__init__.py
+++ bots/telegram-bots/finance-bot/handlers/__init__.py
@@ -1,5 +1,5 @@
 # Add function libraries
 from .system import start
 from .balances import balance
-from cash_boxes import cash_boxes
-from operations import deposit, withdraw
+from .cash_boxes import cash_boxes
+from .operations import deposit, withdraw
